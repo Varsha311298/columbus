4d1b5b0a6f7cf741de774c475f449da7cf70887a
Varsha311298 <varshanatarajan311298@gmail.com>
2020-11-21 02:05:04 +0530

    Add Aws request parser

--- columbus/parser.py
+++ columbus/parser.py
@@ -7,15 +7,92 @@ from columbus.structures import CaseInsensitiveDict
 
 class HttpRequestParser(ABC):
 
+    def __init__(self, context):
+        self.context = context
+
     @abstractmethod
-    def parse_request(self, raw_request):
+    def get_method(self):
         pass
+
+    @abstractmethod
+    def get_path(self):
+        pass
+
+    @abstractmethod
+    def get_mimetype(self):
+        pass
+
+    @abstractmethod
+    def get_params(self):
+        pass
+
+    @abstractmethod
+    def get_body(self):
+        pass
+
+    @abstractmethod
+    def get_headers(self):
+        pass
+
+    def parse_request(self) -> HttpRequest:
+        return HttpRequest(self.get_method(), self.get_path(), self.get_params(),
+                           self.get_body(), self.get_headers(), self.get_mimetype(), self.context)
 
 
 class AWSHttpParser(HttpRequestParser, ABC):
+    def __init__(self, event, context):
+        super().__init__(context)
+        self.event = event
 
-    def parse_request(self, raw_request) -> HttpRequest:
-        pass
+    def get_method(self):
+        return HTTPMethod[self.event['httpMethod']]
+
+    def get_path(self):
+        return self.event['path']
+
+    def get_headers(self):
+        return CaseInsensitiveDict(self.event['headers'])
+
+    def get_mimetype(self):
+        return self.event['headers']['Content-Type'] if self.get_method() is HTTPMethod.PUT or HTTPMethod.POST else None
+
+    def get_body(self):
+        return self.__parse_body()
+
+    def get_params(self):
+        params = {}
+        if self.event.get('queryStringParameters'):
+            params.update(self.event.get('queryStringParameters'))
+        if self.event.get('pathParameters'):
+            params.update(self.event.get('pathParameters'))
+        return params if params else {}
+
+    def __parse_body(self):
+        content_type = self.get_mimetype()
+        body = self.event['body']
+        if 'application/json' in content_type:
+            return json.loads(body)
+        elif 'application/x-www-form-urlencoded' in content_type:
+            return parse_qs(body)
+        elif 'multipart/form-data' in content_type:
+            result = {}
+            name = None
+            filename = None
+            for part in decoder.MultipartDecoder(body.encode(), content_type).parts:
+                form_data = part.headers.get(b'Content-Disposition')
+                key = form_data.decode('utf-8').split("; ")[1:]
+                value = part.text
+                if part.headers.get(b'Content-Type'):
+                    if part.headers.get(b'Content-Type').decode('utf-8') == 'text/html':
+                        name, filename = [x.split('=')[1].strip('/"') for x in key]
+                    if part.headers.get(b'Content-Type').decode('utf-8') == 'text/plain':
+                        name, filename = [x.split('=')[1].strip('/"') for x in key]
+                else:
+                    name = key[0].split("=")[1].strip('/"')
+                result[name] = {value, filename}
+            return result
+        return None
+
 
 
 class LambdaRequestParser:
